9a62259eb3e8d8f2773aaf43c910b755c8301357
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-10-17 05:51:29 +0700

    Forgot to check in file

---
+++ lib/Perinci/CmdLine/Plugin/DumpArgs.pm
@@ -0,0 +1,61 @@
+package Perinci::CmdLine::Plugin::DumpArgs;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# IFUNBUILT
+use strict;
+use warnings;
+# END IFUNBUILT
+use Log::ger;
+
+use parent 'Perinci::CmdLine::PluginBase';
+
+sub meta {
+    return {
+        summary => 'Dump command-line arguments ($r->{args}), by default after argument validation',
+        conf => {
+        },
+    };
+}
+
+sub after_validate_args {
+    require Data::Dump::Color;
+
+    my ($self, $r) = @_;
+
+    Data::Dump::Color::dd($r->{args});
+    [200, "OK"];
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage ^(.+)$
+
+=head1 SYNOPSIS
+
+In the environment variable:
+
+ PERINCI_CMDLINE_PLUGINS=-DumpArgs
+
+In code instantiating L<Perinci::CmdLine>:
+
+ my $app = Perinci::CmdLine::Any->new(
+     ...
+     plugins => ["DumpArgs"],
+ );
+
+If you want to dump at different events:
+
+ my $app = Perinci::CmdLine::Any->new(
+     ...
+     plugins => [
+         'DumpArgs@before_validate_args',
+         'DumpArgs@before_output',
+     ],
+ );
+
+=head1 DESCRIPTION
